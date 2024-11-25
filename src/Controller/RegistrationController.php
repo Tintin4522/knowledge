1d5bd44579e4a5007b5aaebbca9c65527bb9474e
Tintin4522 <kevinpierre4554@gmail.com>
2024-11-25 18:05:20 +0100

    deployment

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -50,7 +50,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('contact@knowledge.com', 'contact bot'))
+                    ->from(new Address('contact@kevinpierre.fr', 'contact'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
