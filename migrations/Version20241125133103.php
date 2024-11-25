4c96e1f3aa8f93d128f75b4faf6a0f237fe60417
Tintin4522 <kevinpierre4554@gmail.com>
2024-11-25 14:45:35 +0100

    deployment

---
+++ migrations/Version20241125133103.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241125133103 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F52993989D86650F');
+        $this->addSql('DROP INDEX IDX_F52993989D86650F ON `order`');
+        $this->addSql('ALTER TABLE `order` CHANGE user_id_id user_id INT NOT NULL');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+        $this->addSql('CREATE INDEX IDX_F5299398A76ED395 ON `order` (user_id)');
+        $this->addSql('ALTER TABLE order_items CHANGE total total NUMERIC(10, 2) DEFAULT \'0\' NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE order_items CHANGE total total NUMERIC(10, 2) DEFAULT \'0.00\' NOT NULL');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A76ED395');
+        $this->addSql('DROP INDEX IDX_F5299398A76ED395 ON `order`');
+        $this->addSql('ALTER TABLE `order` CHANGE user_id user_id_id INT NOT NULL');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F52993989D86650F FOREIGN KEY (user_id_id) REFERENCES users (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('CREATE INDEX IDX_F52993989D86650F ON `order` (user_id_id)');
+    }
+}
